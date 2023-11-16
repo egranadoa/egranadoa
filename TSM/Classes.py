cb410df442f219a793bf18332d714bdca82c715c
Efrain Granado Alfaro <136909816+egranadoa@users.noreply.github.com>
2023-11-16 15:29:13 -0400

    Update Classes.py
    Añadidas clases Technician, Product.
    Corregida Clase padre Person

--- TSM/Classes.py
+++ TSM/Classes.py
@@ -1,17 +1,13 @@
 class Person:
-    def __init__(self, name, phone, place):
+    def __init__(self, name, phone):
         self.name = name
         self.phone = phone
-        self.place = place
     
     def getName(self):
         return self.name
     
     def getPhone(self):
         return self.phone
-    
-    def getPlace(self):
-        return self.place
     
     def setName(self, name):
         self.name = name
@@ -20,12 +16,41 @@ class Person:
     def setPhone(self, phone):
         self.phone = phone
         return self.phone
+      
+      
+class Product:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
     
-    def setName(self, place):
-        self.place = place
-        return self.place
+    def getBrand(self):
+        return self.brand
+    
+    def getModel(self):
+        return self.model
+        
+    def setBrand(self, brand):
+        self.brand = brand
+        return self.brand
+        
+    def setModel(self, model):
+        self.model = model
+        return self.model
     
     
 class Client(Person):
     def __init__(self, name, phone, place):
-        super().__init__(name, phone, place)
+        super().__init__(name, phone)
+        self.place = place
+        
+    def getPlace(self):
+        return self.place
+        
+    def setPlace(self, place):
+        self.place = place
+        return self.place
+
+
+class Technician(Person):
+    def __init__(self, name, phone):
+        super().__init__(name, phone)
