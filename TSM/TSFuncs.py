5c485f300d78fc85df4ab42b47a119f186a8bba5
Efrain Granado Alfaro <136909816+egranadoa@users.noreply.github.com>
2023-11-18 17:34:56 -0400

    Update TSFuncs.py
    Actualizada funcion createTicket para optimizar interaccion con las clases

--- TSM/TSFuncs.py
+++ TSM/TSFuncs.py
@@ -10,6 +10,7 @@ def createCode(ser_num):
 
 def createTicket():
     t_num = 0
+    service = Service()
 
     c_name = str(input("Ingrese el Nombre del Cliente: "))
     c_phone = str(input("Ingrese el Telefono del Cliente: "))
@@ -19,7 +20,7 @@ def createTicket():
     p_brand = str(input("Ingrese la marca del equipo a revisar: "))
     p_model = str(input("Ingrese el Modelo del equipo a revisar: "))
     f_desc = str(input("Indique las fallas que presenta el equipo: "))
-    p_serv = str(input("Especifique el tipo de servicio: "))
+    p_serv = service.selectService()
     t_name = str(input("Ingrese el Nombre del Tecnico a asignar: "))
     t_phone = "0414-0030025"
     t_code = createCode(t_num)
@@ -30,6 +31,4 @@ def createTicket():
 
     tck = Ticket(t_code, datetime.now(), p_serv, f_desc)
     tck.ctd(client, prod, tech)
-
-def closeTicket(Client, Product, Technician, Ticket):
-    print("\n*-FINALIZACION DE TICKET-*")
+    
