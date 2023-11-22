8438d46d40c87a33645dcff79b06c462338a1ef6
Efrain Granado Alfaro <136909816+egranadoa@users.noreply.github.com>
2023-11-22 15:59:30 -0400

    Add files via upload
    Archivo Principal del Sistema

---
+++ TSM/Main.py
@@ -0,0 +1,23 @@
+from Classes import *
+from datetime import datetime
+from TSFuncs import *
+
+program_active = True
+print("TechnicalSupportManagement".center(52, "*"))
+print("Version 0.1".center(52, "-"))
+
+while program_active:
+    menu_sel = int(input("""1) Crear Ticket
+2) Registrar Técnico
+Opción: """))
+    if menu_sel == 1:
+        createTicket()
+    elif menu_sel == 2:
+        rtiDB()
+    else:
+        print("Opción no disponible")
+    eos = str(input("Desea volver a usar el programa? s/n: "))
+    if eos != 's':
+        program_active = False
+    else:
+        pass
