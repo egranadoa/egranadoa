b028a2944c35a2caa17b2c0d94ef449a54f1b123
Efrain Granado Alfaro <136909816+egranadoa@users.noreply.github.com>
2023-11-23 20:32:28 -0400

    Update Main.py
    Eliminada importación innecesaria; agregada selección para crear BD o tablas de BD con el sistema

--- TSM/Main.py
+++ TSM/Main.py
@@ -1,5 +1,4 @@
 from Classes import *
-from datetime import datetime
 from TSFuncs import *
 
 program_active = True
@@ -9,11 +8,14 @@ print("Version 0.1".center(52, "-"))
 while program_active:
     menu_sel = int(input("""1) Crear Ticket
 2) Registrar Técnico
+3) Crear BD/Tablas
 Opción: """))
     if menu_sel == 1:
         createTicket()
     elif menu_sel == 2:
         rtiDB()
+    elif menu_sel == 3:
+        cndb()
     else:
         print("Opción no disponible")
     eos = str(input("Desea volver a usar el programa? s/n: "))
