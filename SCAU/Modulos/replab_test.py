40a8c5ac02c209dabe76efccc9e7b1b626905461
Efrain Granado Alfaro <136909816+egranadoa@users.noreply.github.com>
2024-02-28 07:07:55 -0400

    Update replab_test.py

--- SCAU/Modulos/replab_test.py
+++ SCAU/Modulos/replab_test.py
@@ -1,35 +1,24 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
-from reportlab.lib.enums import TA_JUSTIFY, TA_CENTER
+from reportlab.lib.enums import TA_JUSTIFY
 from reportlab.lib import colors
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Image
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.platypus import Paragraph, Image
+from reportlab.lib.styles import ParagraphStyle
 from reportlab.lib.units import cm
 
-canv = canvas.Canvas("example_flowable.pdf")
+canv = canvas.Canvas("example_flowable.pdf", A4)
 
-styles = getSampleStyleSheet()
-styles.add(ParagraphStyle(name='DOCS',
+
+doc_style = ParagraphStyle(name='DOCS',
                           alignment=TA_JUSTIFY,
                           fontName='Helvetica',
                           fontSize=12,
                           textColor=colors.black,
-                          leading=14,
+                          leading=16,
                           wordWrap='LTR',
                           splitLongWords=False,
-                          spaceShrinkage=0.05,
-                          ))
-
-styles.add(ParagraphStyle(name='TITLES',
-                          alignment=TA_CENTER,
-                          fontName='Helvetica',
-                          fontSize=12,
-                          textColor=colors.black,
-                          leading=14,
-                          wordWrap='LTR',
-                          splitLongWords=False,
-                          spaceShrinkage=0.05,
-                          ))
+                          spaceShrinkage=0.05
+                           )
 
 name = "Carlos"
 surname = "Escalona"
@@ -39,15 +28,25 @@ carreer = 'Informática'
 course = 2
 sem = 4
 
-content = []
-
 header = Image("cintillo_unexca.png", 19*cm, 1.5*cm)
-titles = "<br /><br /><b>CONSTANCIA DE ESTUDIOS</b><br /><br /><b>Núcleo Altagracia</b><br /><br />"
 
-my_text = f"<font size=12>Quien suscribe, ; , hace constar que el(la) ciudadano(a) <b>{name} {surname}</b>, titular de la cédula de identidad N° <b>{nation}-{idc}</b> está inscrito en el <b>Plan Nacional de Formación en {carreer} cursando el Trayecto {course}, Semestre {sem} en el periodo academico 2024-1</b>\n\nConstancia que se expide a petición de la parte interesada en Caracas, 25 de febrero de 2024.</font>"
+canv.setFont('Helvetica-Bold', 12)
+canv.drawCentredString(290, 700, 'CONSTANCIA DE ESTUDIOS')
+canv.drawCentredString(290, 675, 'Núcleo Altagracia')
 
-doc = SimpleDocTemplate("example_flowable.pdf",pagesize=A4,
-                        rightMargin=2*cm,leftMargin=2*cm,
-                        topMargin=0.1*cm,bottomMargin=2*cm)
+p1 = Paragraph(f"<font size=12>Quien suscribe, ; , hace constar que el(la) ciudadano(a) <b>{name} {surname}</b>, titular de la cédula de identidad N° <b>{nation}-{idc}</b> está inscrito en el <b>Plan Nacional de Formación en {carreer} cursando el Trayecto {course}, Semestre {sem} en el periodo academico 2024-1</b><br /><br />Constancia que se expide a petición de la parte interesada en Caracas, 25 de febrero de 2024.</font>", doc_style)
 
-doc.build([header, Paragraph(titles, styles['TITLES']), Paragraph(my_text.replace("\n", "<br />"), styles['DOCS']),])
+canv.setFont('Helvetica', 12)
+canv.drawCentredString(290, 475, 'Atentamente,')
+
+header.drawOn(canv, 25, 790)
+p1.wrapOn(canv, 550, 125)
+p1.drawOn(canv, 25, 575)
+
+page_number = canv.getPageNumber()
+p_num = 'Página %s' % page_number
+
+canv.setFont('Helvetica', 8)
+canv.drawString(25, 10, p_num)
+
+canv.save()
